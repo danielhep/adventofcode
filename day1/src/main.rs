8f296796b9256bbd141f87e43719bb35dc63841c
Daniel Heppner <dh@danielheppner.com>
2023-12-02 14:41:59 -0800

    finish day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,29 +1,52 @@
-use std::fs;
 use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
 
 fn main() {
+    // Make the look up table
+    let string_int_vals: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9)
+    ]);
+
     let contents = fs::read_to_string("day1/src/input.txt").expect("File contents not read");
     let lines = contents.split("\n");
     let mut sum = 0;
 
-    let first_digit_re = Regex::new(r"^.*?(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))").unwrap();
-    let last_digit_re = Regex::new(r".*(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)).*$").unwrap();
+    let first_digit_re =
+        Regex::new(r"^.*?(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))")
+            .unwrap();
+    let last_digit_re =
+        Regex::new(r".*(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)).*$")
+            .unwrap();
+    let regexes = [first_digit_re, last_digit_re];
     for line in lines.into_iter() {
-        let mut digit1 = first_digit_re.captures(line).unwrap().get(1).unwrap().as_str();
-        let mut digit2 = last_digit_re.captures(line).unwrap().get(1).unwrap().as_str();
-        for character in line.chars() {
-            // Look for a digit
-            if character.is_digit(10) {
-                if digit1.is_none() {
-                    digit1 = Option::Some(character.to_digit(10).unwrap())
-                } else {
-                    digit2 = Option::Some(character.to_digit(10).unwrap())
-                }
+        let captures = regexes.clone().map(|regex| {
+            regex
+                .captures(line)
+                .unwrap()
+                .get(1)
+                .expect("no digit found!")
+                .as_str()
+        });
+
+        let digits = captures.map(|c| {
+            if c.chars().nth(0).unwrap().is_digit(10) {
+                c.chars().nth(0).unwrap().to_digit(10).unwrap()
+            } else {
+                *string_int_vals.get(c).unwrap()
             }
-        }
-        sum += digit1.unwrap() * 10 + digit2.unwrap_or(digit1.unwrap());
+        });
+
+        sum += digits[0] * 10 + digits[1];
     }
 
     println!("Total value: {}", sum);
 }
-
