117f7c7ea39c56421f5c9941aedd40f5c27d5d01
danielhep <dh@danielheppner.com>
2023-12-02 12:06:29 -0800

    start work on day 1 pt 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,16 +1,19 @@
 use std::fs;
+use regex::Regex;
 
-const CHARACTER_DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 fn main() {
     let contents = fs::read_to_string("day1/src/input.txt").expect("File contents not read");
     let lines = contents.split("\n");
     let mut sum = 0;
+
+    let first_digit_re = Regex::new(r"^.*?(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))");
+    let last_digit_re = Regex::new(r".*(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)).*$");
     for line in lines.into_iter() {
         let mut digit1: Option<u32> = Option::None;
         let mut digit2: Option<u32> = Option::None;
         for character in line.chars() {
             // Look for a digit
-            if CHARACTER_DIGITS.contains(&character) {
+            if character.is_digit(10) {
                 if digit1.is_none() {
                     digit1 = Option::Some(character.to_digit(10).unwrap())
                 } else {
